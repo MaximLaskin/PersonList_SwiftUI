8c8cef495aa380ae08b9e9f3681033a9176edd9b
MaximLaskin <maxlaskin@mail.ru>
2022-11-02 23:41:32 +0300

    Initial Commit

---
+++ PersonList_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PersonList_SwiftUI
+//
+//  Created by Maxim on 02.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PersonList_SwiftUI/PersonList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonList_SwiftUIApp.swift
+//  PersonList_SwiftUI
+//
+//  Created by Maxim on 02.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
